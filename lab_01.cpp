ad37bbfe1070fa7aea4573b56277d38b81beaab1
Bahkir A.D <Bakhir.A.D.st@gmail.com>
2020-02-12 09:51:26 +0300

    no tabs anymore

--- lab_01.cpp
+++ lab_01.cpp
@@ -10,24 +10,24 @@ int main(int argc, char* argv[])
     pthread_attr_t my_attr; 
     pthread_attr_init(&my_attr);
     pthread_create(&my_thread, &my_attr, some_func, argv[1]);
-	for(int i = 0; i < 5; i++)
-	{
-		sleep(2);
-		cout << "1";
-	}
-		
-	
+    for(int i = 0; i < 5; i++)
+    {
+        sleep(2);
+        cout << "1";
+    }
+        
+    
     pthread_join(my_thread, nullptr);
-	cout << endl;
+    cout << endl;
     return 0;
 }
 
 void* some_func(void * some)
 {
     for(int i = 0; i<10; i++)
-	{
-		sleep(1);
-		cout << "2";
-	}
+    {
+        sleep(1);
+        cout << "2";
+    }
     pthread_exit(0);
 }
