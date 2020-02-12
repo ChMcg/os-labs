a98e6a7165f4efd0d1c278ce726960b3763e5e3b
Bahkir A.D <Bakhir.A.D.st@gmail.com>
2020-02-12 10:17:50 +0300

    attr 2 removed | lab_01 done

--- lab_01.cpp
+++ lab_01.cpp
@@ -13,11 +13,10 @@ int main(int argc, char* argv[])
     pthread_t my_thread_1, my_thread_2;
     bool p1_work = true;
     bool p2_work = true;
-    pthread_attr_t my_attr_1, my_attr_2; 
+    pthread_attr_t my_attr_1; 
     pthread_attr_init(&my_attr_1);
-    pthread_attr_init(&my_attr_2);
     pthread_create(&my_thread_1, &my_attr_1, some_p1, (void*)&p1_work);        
-    pthread_create(&my_thread_2, &my_attr_2, some_p2, (void*)&p2_work);        
+    pthread_create(&my_thread_2, &my_attr_1, some_p2, (void*)&p2_work);        
     
     getch();
     p1_work = false;
