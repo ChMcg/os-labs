19fa481748a4407847c17a60c62e0738fbf73ee1
Bahkir A.D <Bakhir.A.D.st@gmail.com>
2020-02-19 16:13:24 +0300

    fix | lab_02 done

--- lab_02.cpp
+++ lab_02.cpp
@@ -24,8 +24,8 @@ int main(int argc, char* argv[])
     pthread_mutex_init(&mutex, nullptr);
     p_args p1_arg = {true, &mutex};
     p_args p2_arg = {true, &mutex};
-    pthread_create(&my_thread_1, &my_attr_1, some_p1, (void*)&p1_arg);        
-    pthread_create(&my_thread_2, &my_attr_1, some_p2, (void*)&p2_arg);        
+    pthread_create(&my_thread_1, &my_attr_1, some_p1, (void*)&p1_arg);
+    pthread_create(&my_thread_2, &my_attr_1, some_p2, (void*)&p2_arg);
     getch();
     p1_arg.work = false;
     p2_arg.work = false;
